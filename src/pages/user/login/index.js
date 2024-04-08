a91ccaa417ef76e6e31ec5c7b3b013323e9e32a4
Trần Tiến Thế <tranthe2k2ak@gmail.com>
2024-04-08 22:36:18 +0700

    the: login success

--- src/pages/user/login/index.js
+++ src/pages/user/login/index.js
@@ -1,33 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiUser, CiLock } from "react-icons/ci";
 import { Bounce, ToastContainer, toast } from "react-toastify";
 import ToastNotify from "pages/component/ToastNotify";
 import { useNavigate } from "react-router-dom";
+import ReactLoading from "react-loading";
 
 const Login = () => {
   const navigate = useNavigate();
-  const handleLogin = () => {
-    navigate("/", {
-      state: {
-        notify: {
-          type: "success",
-          message: "Xin chào bạn",
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    setLoading(true);
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-      },
-    });
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        localStorage.setItem("isLoggedIn", "true");
+        localStorage.setItem("username", username);
+
+        navigate("/", {
+          state: {
+            notify: {
+              type: "success",
+              message: "Xin chào bạn",
+            },
+          },
+        });
+      } else {
+        const errorData = await response.json();
+        toast.error("Vui lòng kiểm tra lại !");
+      }
+    } catch (error) {
+      console.error("Đã có lỗi xảy ra:", error);
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <div className="containers">
       <ToastNotify />
       <div className="wrapper">
         <form action="">
-          <h1>Login</h1>
+          <h1>
+            Đăng nhập{" "}
+            {loading && (
+              <div style={{ display: "inline-block", marginLeft: "20px" }}>
+                <ReactLoading
+                  type="spin"
+                  color="#00BFFF"
+                  height={20}
+                  width={20}
+                />
+              </div>
+            )}
+          </h1>
           <div className="input-box">
-            <input type="text" placeholder="Email" required />
+            <input id="username" type="text" placeholder="Tài khoản" required />
             <CiUser className="icon" />
           </div>
           <div className="input-box">
-            <input type="password" placeholder="Password" required />
+            <input
+              id="password"
+              type="password"
+              placeholder="Mật khẩu"
+              required
+            />
             <CiLock className="icon" />
           </div>
           <button type="button" onClick={handleLogin}>

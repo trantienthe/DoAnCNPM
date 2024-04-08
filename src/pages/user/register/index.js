8a1de01ecf5694cdf3f72c6170bfad5afd9401be
Trần Tiến Thế <tranthe2k2ak@gmail.com>
2024-04-08 21:03:40 +0700

    the: handle register

--- src/pages/user/register/index.js
+++ src/pages/user/register/index.js
@@ -1,47 +1,168 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 import { CiUser, CiLock } from "react-icons/ci";
 import ToastNotify from "pages/component/ToastNotify";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import ReactLoading from "react-loading";
 
 const Register = () => {
   const navigate = useNavigate();
-  const handleRegister = () => {
-    navigate("/", {
-      state: {
-        notify: {
-          type: "success",
-          message: "Đăng ký thành công",
-        },
-      },
-    });
+
+  const [formData, setFormData] = useState({
+    email: "",
+    last_name: "",
+    first_name: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    if (!isValidEmail(formData.email)) {
+      toast.error("Email không hợp lệ !!!");
+      return;
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      toast.error("Mật khẩu nhập không khớp !!!");
+      return;
+    }
+    delete formData.confirmPassword;
+
+    setLoading(true);
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/register/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setTimeout(() => {
+          setLoading(false);
+          navigate("/", {
+            state: {
+              notify: {
+                type: "success",
+                message: "Đăng ký thành công!",
+              },
+            },
+          });
+        }, 2000);
+        console.log(data);
+      } else {
+        const errorData = await response.json();
+        alert(errorData.message);
+      }
+    } catch (error) {
+      console.error("Đã có lỗi xảy ra:", error);
+    }
+  };
+
   return (
     <div className="containers">
       <ToastNotify />
 
       <div className="wrapper">
-        <form action="">
-          <h1>Register</h1>
+        <form onSubmit={handleRegister}>
+          <h1>
+            Đăng ký{" "}
+            {loading && (
+              <div style={{ display: "inline-block", marginLeft: "20px" }}>
+                <ReactLoading
+                  type="spin"
+                  color="#00BFFF"
+                  height={20}
+                  width={20}
+                />
+              </div>
+            )}
+          </h1>
+
           <div className="input-box">
-            <input type="text" placeholder="Họ và Tên" required />
+            <input
+              type="text"
+              name="first_name"
+              placeholder="Họ"
+              value={formData.first_name}
+              onChange={handleChange}
+              required
+            />
             <CiUser className="icon" />
           </div>
           <div className="input-box">
-            <input type="text" placeholder="Email" required />
+            <input
+              type="text"
+              name="last_name"
+              placeholder="Tên"
+              value={formData.last_name}
+              onChange={handleChange}
+              required
+            />
             <CiUser className="icon" />
           </div>
           <div className="input-box">
-            <input type="password" placeholder="Password" required />
+            <input
+              type="text"
+              name="username"
+              placeholder="Tên đăng nhập"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+            <CiUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <CiUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input
+              type="password"
+              name="password"
+              placeholder="Mật khẩu"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
             <CiLock className="icon" />
           </div>
           <div className="input-box">
-            <input type="password" placeholder="Confirm Password" required />
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Nhập lại mật khẩu"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+            />
             <CiLock className="icon" />
           </div>
-          <button type="submit" onClick={handleRegister}>
-            Register
-          </button>
+          <button type="submit">Register</button>
 
           <div className="register-link">
             <p>
